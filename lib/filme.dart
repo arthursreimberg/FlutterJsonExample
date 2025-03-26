eee9bd0cceb93feec12261431cc355615303c78c
Arthur <arthurreimberg21@gmail.com>
2025-03-26 12:09:34 -0300

    inicial

---
+++ lib/filme.dart
@@ -0,0 +1,36 @@
+import 'dart:ffi';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+class Filme
+{
+
+  late int id;
+  late String nome;
+  late int ano;
+  late double avaliacao;
+  late String diretor;
+
+
+  Filme()
+  {
+    id = 0;
+    nome ="";
+    ano = 0;
+    avaliacao = 0.0;
+    diretor = "";
+
+  }
+
+  Filme.v(this.id, this.nome, this.ano, this.avaliacao, this.diretor);
+
+  //Mapeando a classe com o Json
+  Filme.fromJson(Map<String, dynamic> json): 
+    id = json["idImdb"] as int,
+    nome = json["title"] as String,
+    ano = json["year"] as int,
+    avaliacao = json["rating"] as double,
+    diretor = json["director"] as String;
+
+ 
+}
