52b63e4381756d9d082c45a6ea3d9a920009021e
Arthur <arthurreimberg21@gmail.com>
2025-04-02 12:13:20 -0300

    Apresentação da lista

--- lib/main.dart
+++ lib/main.dart
@@ -4,60 +4,53 @@ import 'package:flutter_cinema/filme.dart';
 import 'dart:convert';
 
 void main() {
-  runApp( const MaterialApp (title: "App",
-      home: MainApp(),));;
+  runApp(const MaterialApp(title: "App", home: MainApp()));
 }
 
-
-class MainApp extends StatefulWidget{
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
-  
+
   @override
   MainAPP createState() => MainAPP();
 }
-  
 
 class MainAPP extends State {
   List<Filme> filmes = List.empty();
+  int total = 0;
   Future<void> readJson() async {
     var rootBundle2 = rootBundle;
     final String response = await rootBundle2.loadString('assets/movies.json');
     Iterable data = await json.decode(response);
-    filmes =  List<Filme>.from(data.map((model)=> Filme.fromJson(model)));
+    filmes = List<Filme>.from(data.map((model) => Filme.fromJson(model)));
+    total = filmes.length;
     setState(() {
       filmes;
+      total;
     });
   }
-  
+
   @override
-   initState()  {
+  initState() {
     super.initState();
-       readJson();
-       }
+    readJson();
+  }
+
   @override
   Widget build(BuildContext context) {
-    int total = 50;
     return MaterialApp(
       home: Scaffold(
+        extendBody: true,
         body: Center(
-          child: ListView.builder(
-            itemExtent: total.toDouble(), 
-            itemBuilder: (BuildContext context, int index) { 
-              return Column(
-                children: [
-                  Text(filmes[index].id as String),
-                  Text(filmes[index].nome),
-                  Text(filmes[index].ano as String),
-                  Text(filmes[index].avaliacao as String),
-                  Text(filmes[index].diretor)
-                ],
-              );
-             },
-            
-
-          )
-        ),
-      ),
+          child: 
+                ListView.builder(
+                  itemCount: filmes.length,
+                  itemBuilder: (BuildContext context, index)
+                   {  
+                    return  Text(
+                        filmes[index].nome
+                      );
+                   },)
+            )),
     );
   }
 }
